d5ea124647b64cef7ff7855c9f872893ae31a3d7
Ertiops <104155685+Ertiops@users.noreply.github.com>
2023-08-24 15:03:37 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 import crud
+import sys
+
+# provides correct cyrillic symbols output
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
 
 # reads description from txt file
 with open('description.txt', 'r', encoding="utf-8") as file:
